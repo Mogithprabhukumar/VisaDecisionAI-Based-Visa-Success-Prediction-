6c151afde29b708abe7b760e3edba5595c4af0e9
Mogith <mogith.p2004b1@gmail.com>
2026-04-28 22:34:44 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Abhishek Chauhan",
+    author_email="chauhanabhishek611@gmail.com",
+    packages=find_packages()
+)
