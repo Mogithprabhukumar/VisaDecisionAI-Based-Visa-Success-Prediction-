0c64e7733882dc42180af1f67b3684ee72c66f88
Mogith <mogith.p2004b1@gmail.com>
2026-04-28 22:39:17 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup,find_packages
-
-setup(
-    name="us_visa",
-    version="0.0.0",
-    author="Abhishek Chauhan",
-    author_email="chauhanabhishek611@gmail.com",
-    packages=find_packages()
-)
